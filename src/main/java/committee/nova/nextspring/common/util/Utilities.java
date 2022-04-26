325415ad2b237bdb157b180c28b467b528c70562
MikhailTapio <mikhailtapio13468@163.com>
2022-04-26 11:26:02 +0800

    Port to LegacyFabric 1.12.2

---
+++ src/main/java/committee/nova/nextspring/common/util/Utilities.java
@@ -0,0 +1,18 @@
+package committee.nova.nextspring.common.util;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.Growable;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class Utilities {
+    public static boolean tryCatalyze(World world, BlockPos pos) {
+        final Block block = world.getBlockState(pos).getBlock();
+        if (!(block instanceof Growable)) return false;
+        final Growable growable = (Growable) block;
+        if (!growable.canGrow(world, pos, world.getBlockState(pos), world.isClient)) return false;
+        if (!world.isClient && growable.canBeFertilized(world, world.random, pos, world.getBlockState(pos)))
+            growable.grow(world, world.random, pos, world.getBlockState(pos));
+        return true;
+    }
+}
